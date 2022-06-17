b8df5dc476463a1da313c3be4d9ceec0f15b922c
Kathleen Prasatko <88250122+cult4nthropo@users.noreply.github.com>
2022-06-17 15:14:20 +0200

    initial commit

---
+++ src/main/java/de/tfckn/mitglieder/MitgliederApplication.java
@@ -0,0 +1,13 @@
+package de.tfckn.mitglieder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitgliederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitgliederApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/tfckn/mitglieder/MitgliederApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tfckn.mitglieder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitgliederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
